7f4c05b0eacc7e43aa1ced971d5e3ad81112edf3
David N. Mashburn <david.n.mashburn@gmail.com>
2009-10-07 01:10:50 -0500

    Version 0.9.8.0.5

--- symbolConversion.py
+++ symbolConversion.py
@@ -8,22 +8,47 @@ from ast import Call, Load, Name, copy_location
 
 nameAddOn = 'SYMPYSL_'
 
-class BinOp2Function(ast.NodeTransformer):
+# Ok, this will work...
+# First, build a list of all the BinOps left&right positions using a NodeVisitor
+# Next, find the BinOp that has left<myop<right , but has right-left be a minimum
+# Now, save THIS left-right pair to a list with the appropriate dictionary name for each operator
+# and use this dictionary as the condition for matching operators (left,right == dict.leftright)
+# Hooray, might work!
+class BinOpRowColCollector(ast.NodeVisitor):
+    def __init__(self):
+        self.rowsCols = []
+    
     def visit_BinOp(self, node):
-        inside = False
-        for rc in self.rowsCols:
-            if node.left.lineno <= rc[0] <= node.right.lineno and \
-               node.left.col_offset < rc[1] < node.right.col_offset:
-                inside=True
-            
-        if inside:
-            return copy_location(
-                      Call(func=Name(self.funcName, Load(),lineno=node.lineno,col_offset=node.col_offset),
+        self.generic_visit(node)
+        self.rowsCols .append([ [node.left.lineno, node.left.col_offset],
+                                [node.right.lineno,node.right.col_offset] ])
+
+class BinOp2Function(ast.NodeTransformer):
+    def __init__(self):
+        self.funcName = None
+        self.astOp = None
+        self.coords=[]
+    def visit_BinOp(self, node):
+        self.generic_visit(node)
+        
+        print node.op,node.left.col_offset,node.right.col_offset
+        print node.left.lineno, node.left.col_offset,node.right.lineno,node.right.col_offset
+        for c in self.coords:
+            if node.left.lineno == c[0][0] and \
+               node.left.col_offset == c[0][1] and \
+               node.right.lineno == c[1][0] and \
+               node.right.col_offset == c[1][1]:
+                if self.astOp != type(node.op):
+                    print 'Parsing Error!!'
+                print 'DONE!'
+                return copy_location(Call(
+                           func=Name(self.funcName, Load(),lineno=node.lineno,
+                                     col_offset=node.col_offset),
                            args=[node.left, node.right],
                            keywords=[],
                            lineno=node.lineno,
                            col_offset=node.col_offset),
-                      node)
+                       node)
         else:
             return node
 
@@ -36,19 +61,46 @@ def GetOperatorRowCol(s,names):
             while p.find(nameAddOn+name+'_') != -1:
                 col = p.find(nameAddOn+name+'_')
                 rowCol[name].append([row+1,p.find(nameAddOn+name+'_')])
-                p = p.replace(nameAddOn+name+'_','+',1)
+                p = p.replace(nameAddOn+name+'_',n2precedence[name],1) # Take old ones out of the mix...
     return rowCol
 
 def ASTWithConversion(s):
     names = operator_names
     rowCol = GetOperatorRowCol(s,names)
     for name in names:
-        s = s.replace(nameAddOn+name+'_','+')
+        s = s.replace(nameAddOn+name+'_',n2precedence[name])
+    print s
     mod=ast.parse(s,mode='single')
+    borcc = BinOpRowColCollector()
+    borcc.visit(mod)
+    
+    coords = {}
+    
+    for name in names:
+        coords[name] = []
+        for i,rc in enumerate(rowCol[name]):
+            coords[name].append([]) # coords[name][i] = []
+            for rl in borcc.rowsCols:
+                # If rc is within rl...
+                #  left.lineno          right.lineno
+                #  left.col_offset    right.col_offset
+                if rl[0][0] <= rc[0] <= rl[1][0] and \
+                   rl[0][1] < rc[1] < rl[1][1]:
+                    if coords[name][i] == []:
+                        coords[name][i] = rl
+                    # If this rl is tighter than coords[name][i]
+                    elif rl[0][0] >= coords[name][i][0][0] and \
+                         rl[0][1] >= coords[name][i][0][1] and \
+                         rl[1][1] <= coords[name][i][0][1] and \
+                         rl[1][1] <= coords[name][i][0][1]:
+                        coords[name][i] = rl
+    
+    print coords
     bo2f=BinOp2Function()
     for name in names:
         bo2f.funcName = '__'+name+'__'
-        bo2f.rowsCols=rowCol[name]
+        bo2f.astOp = precedence2astName[n2precedence[name]]
+        bo2f.coords=coords[name]
         mod = bo2f.visit(mod)
     return mod
 
@@ -75,13 +127,32 @@ greek_names = ['alpha', 'beta', 'gamma', 'delta', 'epsilon', 'zeta', 'eta', 'the
 GREEK_names = ['Alpha', 'Beta', 'Gamma', 'Delta', 'Epsilon', 'Zeta', 'Eta', 'Theta', 'Iota', 'Kappa', 'Lambda', 'Mu', 'Nu', 'Xi', 'Omicron', 'Pi', 'Rho', 'Sigma', 'Tau', 'Upsilon', 'Phi', 'Chi', 'Psi', 'Omega']
 
 operator_names = ['dot', 'cross']
+operatorSymbolForPrecedence = ['*','*']
+# Dictionary to convert symbols to ast Names, in order of precedence
+precedence2astName = {
+'or' : ast.Or,
+'and': ast.And,
+'==' : ast.Eq,
+'|'  : ast.BitOr,
+'^'  : ast.BitXor,
+'&'  : ast.BitAnd,
+'<<' : ast.RShift,
+'+'  : ast.Add,
+'*'  : ast.Mult,
+'**' : ast.Pow
+}
+
 
 allNames = greek_names + GREEK_names + operator_names
 allExpandedAsciiNames = [nameAddOn + i + '_' for i in allNames]
 expandedOperatorAsciiNames = [nameAddOn + i + '_' for i in operator_names]
 
+# Operator Conversion dictionaries
 n2o = dict([[operator_names[i],operators[i]] for i in range(len(operators))])
+n2o['.']=n2o['dot']
+
 o2n = dict([[operators[i],operator_names[i]] for i in range(len(operators))])
+n2precedence = dict([[operator_names[i],operatorSymbolForPrecedence[i]] for i in range(len(operators))])
 
 # Dictionaries to convert from Greek names to Greek characters
 n2g = dict([[greek_names[i],greek[i]] for i in range(24)])
@@ -90,7 +161,7 @@ N2G = dict([[GREEK_names[i],GREEK[i]] for i in range(24)])
 g2n = dict([[greek[i],greek_names[i]] for i in range(24)])
 G2N = dict([[GREEK[i],GREEK_names[i]] for i in range(24)])
 
-# Dictionaries for "shorthand" conversion from 1 and 2 letter combinations to full Greek names
+# Dictionaries for "shorthand" conversion from 1 and 2 letter combinations to full symbol names
 a2n = {
 'a' : 'alpha',
 'b' : 'beta',
