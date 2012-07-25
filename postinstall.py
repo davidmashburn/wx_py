18f13cdec88f952f08203c8b4d3f86e67e9e06f1
David N. Mashburn <david.n.mashburn@gmail.com>
2012-07-25 17:48:34 -0500

    Version 0.9.8.11 -- Changed postinstall.py to check for windows w/sys.platform

--- postinstall.py
+++ postinstall.py
@@ -10,28 +10,30 @@ import imp
 
 modulePath = imp.find_module('wx_py')[1]
 
-DESKTOP_FOLDER = get_special_folder_path("CSIDL_DESKTOPDIRECTORY")
+if sys.platform == "win32":
+    from postinstall import get_special_folder_path
+    DESKTOP_FOLDER = get_special_folder_path("CSIDL_DESKTOPDIRECTORY")
 
-if sys.argv[1] == '-install':
-    for shellName in ['PyCrust','PySlices','SymPySlices']:
-        lnkName=shellName+'.lnk'
-        pythonexe = ('python.exe' if shellName=='SymPySlices' else 'pythonw.exe')
-        create_shortcut(
-            os.path.join(sys.prefix, pythonexe), # program
-            shellName, # description
-            lnkName, # filename
-            os.path.join(modulePath, shellName+'.py'), # parameters
-            '', # workdir
-            os.path.join(modulePath, shellName+'.ico'), # iconpath
-        )
-        # move shortcut from current directory to DESKTOP_FOLDER
-        shutil.move(os.path.join(os.getcwd(), lnkName),
-                    os.path.join(DESKTOP_FOLDER, lnkName))
-        # tell windows installer that we created another
-        # file which should be deleted on uninstallation
-        file_created(os.path.join(DESKTOP_FOLDER, lnkName))
+    if sys.argv[1] == '-install':
+        for shellName in ['PyCrust','PySlices','SymPySlices']:
+            lnkName=shellName+'.lnk'
+            pythonexe = ('python.exe' if shellName=='SymPySlices' else 'pythonw.exe')
+            create_shortcut(
+                os.path.join(sys.prefix, pythonexe), # program
+                shellName, # description
+                lnkName, # filename
+                os.path.join(modulePath, shellName+'.py'), # parameters
+                '', # workdir
+                os.path.join(modulePath,'icons',shellName+'.ico'), # iconpath
+            )
+            # move shortcut from current directory to DESKTOP_FOLDER
+            shutil.move(os.path.join(os.getcwd(), lnkName),
+                        os.path.join(DESKTOP_FOLDER, lnkName))
+            # tell windows installer that we created another
+            # file which should be deleted on uninstallation
+            file_created(os.path.join(DESKTOP_FOLDER, lnkName))
 
-if sys.argv[1] == '-remove':
-    pass
-    # This will be run on uninstallation. Nothing to do.
+    if sys.argv[1] == '-remove':
+        pass
+        # This will be run on uninstallation. Nothing to do.
 
